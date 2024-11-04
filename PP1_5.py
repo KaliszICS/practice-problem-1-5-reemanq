31d3ec858ee8ab25e23e5e688fab3da3989f518e
reemanq <reem.ansar@student.tdsb.on.ca>
2024-11-04 17:39:07 -0500

    Update PP1_5.py

--- PP1_5.py
+++ PP1_5.py
@@ -1,17 +1,28 @@
 
 def q1():
+  a = input("Input an integer: ")
+  print(int(a) + 3)
   #Write Assignment code here
 
 def q2():
+  b = float(input("Input a number: ")) + 4
+  print(float(b) + 2)
   #Write Assignment code here
 
 def q3():
+  c = float(input("Input a radius: "))
+  c = 3.14 * c * c
+  print(c)
   #Write Assignment code here
 
 def q4():
+  d = float(input("Input a number: ")) * 12
+  print(round(d))
   #Write Assignment code here
 
 def q5():
+  e = int(input("Input an integer: ")) + 5
+  print("Your number + 5 is",e)
   #Write Assignment code here
 
 #Comment this code out when running tests
