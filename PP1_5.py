65637bb6aa5e5bcff4c6e1330761f83d9b2fe000
reemanq <reem.ansar@student.tdsb.on.ca>
2024-11-04 20:51:12 -0500

    Update PP1_5.py

--- PP1_5.py
+++ PP1_5.py
@@ -28,8 +28,8 @@ def q5():
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
